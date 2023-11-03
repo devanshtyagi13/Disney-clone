0ae68093cd3c2299772de5866a40eea450cbddc3
devanshtyagi13 <66518032+devanshtyagi13@users.noreply.github.com>
2023-11-03 17:12:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepyuri, eaque voluptatem impedit nulla laboriosam facilis ut  laboriosam libero!',
+        image: 'images/slider 2.png'
+    },
+    {
+        name: 'loki',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepyuri, eaque voluptatem impedit nulla laboriosam facilis ut  laboriosam libero!',
+        image: 'images/slider 1.png'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepyuri, eaque voluptatem impedit nulla laboriosam facilis ut  laboriosam libero!',
+        image: 'images/slider 3.png'
+    },
+]
